cf8dc7e20f564376450a8dc1c303ef694fdf7de5
Willy Angga <ampedwilly@gmail.com>
2023-01-25 11:49:31 +0700

    Setup database

--- src/main/java/com/willy/facebox/repisotory/PersonRepository.java
+++ src/main/java/com/willy/facebox/repisotory/PersonRepository.java
@@ -1,4 +1,9 @@
 package com.willy.facebox.repisotory;
 
-public interface PersonRepository {
+import com.willy.facebox.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer> {
 }
