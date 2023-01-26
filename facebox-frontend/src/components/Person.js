2162f37c13c965be2b8f22ac8cab5a8805442f34
Willy Angga <ampedwilly@gmail.com>
2023-01-26 15:30:14 +0700

    Integrate fetch with backend

--- facebox-frontend/src/components/Person.js
+++ facebox-frontend/src/components/Person.js
@@ -7,6 +7,23 @@ export default function Person() {
   const paperStyle = {padding : '50px 20px', width : 600, margin: "20px auto" }
   const[name, setName] = React.useState('')
   const[address, setAddress] = React.useState('')
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    const person = {name, address}
+    fetch(
+      "http://localhost:8080/person/add", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(person)
+      }
+    ).then(() => {
+      alert(`${name} has been added`)
+      setName('')
+      setAddress('')
+    })
+  }
+
   return (
     <Container>
       <Paper elevation = {3} style = {paperStyle}>
@@ -18,16 +35,18 @@ export default function Person() {
           }}
           noValidate
           autoComplete="off">
+
           <TextField id="outlined-basic" label="Name" variant="outlined" fullWidth 
           value = {name}
           onChange = {(e) => setName(e.target.value)}
           />
+
           <TextField id="outlined-basic" label="Address" variant="outlined" fullWidth
           value = {address}
           onChange = {(e) => setAddress(e.target.value)}
           />
-          <Button variant="contained" color = "primary">
-            Submit
+          <Button variant="contained" color = "primary" onClick = {handleClick}>
+          Submit
           </Button>
         </Box>
       </Paper>
